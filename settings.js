1a50bc0357349e3fb154f7c30c21ba94fd5f512d
Hkcyberbot <114365823+Hkcyberbot@users.noreply.github.com>
2022-09-26 05:36:52 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -51,7 +51,7 @@ global.owner = ['+6289508996342']
 global.ownertag = '+6289508996342' //your tag number
 global.botname = 'KNET - 𝘉𝘖𝘛' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
+global.author = "KNET BOT" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
 global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
 
